7c15abb7f0bbd540ee2dadafb09d8d3817a361f4
Vinay Chandil <vinaychandil17@gmail.com>
2017-08-03 15:33:32 +0530

    Second

---
+++ index.php
@@ -0,0 +1,32 @@
+<html>
+<head>
+<link rel="stylesheet" type="text/css" href="semantic.css">
+<script
+  src="https://code.jquery.com/jquery-3.1.1.min.js"
+  integrity="sha256-hVVnYaiADRTO2PzUGmuLJr8BLUSjGIZsDYGmIJLv2b8="
+  crossorigin="anonymous"></script>
+<script src="semantic.min.js"></script>
+<link href="https://fonts.googleapis.com/css?family=Orbitron" rel="stylesheet">
+<link rel="stylesheet" href="main.css">
+<link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">
+</head>
+<body>
+<div class="ui stackable menu" >
+  <div class="item">
+    <a href="http://www.allprix.com"><img src="img/logo.png" style="width:100px" title="allprix">
+  </div>
+  <a class="item" style="
+    font-size: xx-large;margin-left: 80%;font-family: 'Montserrat', sans-serif;
+    color: #fbc02d;">Blog</a>
+</div>
+<div class="container">
+<img width="50%" src="img/logorect.png" style="margin-left: 25%;margin-top:5%" title="allprix">
+</div>
+<div class="ui sizer vertical segment" style="text-align:center">
+  <div class="ui huge header" style="color:chocolate">Coming Soon to serve you better</div>
+  </div>
+  <!--<div class="ui sizer vertical segment">
+  &copy;Copyright 2017
+  </div> -->
+</body>
+</html>
